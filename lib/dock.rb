7c0c700eb74f71f01a70df09df158a46b14608e6
Darren Campbell <darren2802@gmail.com>
2019-10-22 18:59:19 -0600

    Adds iterations 3 and 4

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,15 +1,34 @@
 class Dock
 
-  attr_reader :name, :max_rental_time, :rental_log
+  attr_reader :name, :max_rental_time, :rental_log, :revenue
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @rental_log = Hash.new
+    @revenue = 0
   end
 
   def rent(boat, renter)
     @rental_log[boat] = renter
+  end
+
+  def charge(boat)
+    renter = @rental_log[boat]
+    total_charge = [boat.hours_rented * boat.price_per_hour, max_rental_time * boat.price_per_hour].min
+    charge = {
+      :card_number => renter.credit_card_number,
+      :amount => total_charge
+    }
+  end
+
+  def log_hour
+    @rental_log.each_key { |key| key.add_hour }
+  end
+
+  def return(boat)
+    @revenue += charge(boat)[:amount]
+    @rental_log.delete(boat)
   end
 
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -9,10 +9,11 @@ class TestDock < Minitest::Test
     @dock = Dock.new("The Rowing Dock", 3)
     @kayak_1 = Boat.new(:kayak, 20)
     @kayak_2 = Boat.new(:kayak, 20)
+    @canoe = Boat.new(:canoe, 25)
     @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
     @patrick = Renter.new("Patrick Star", "4242424242424242")
     @eugene = Renter.new("Eugene Crabs", "1313131313131313")
-
   end
 
   def test_dock_initialized
@@ -31,6 +32,55 @@ class TestDock < Minitest::Test
     @dock.rent(@kayak_2, @patrick)
     @dock.rent(@sup_1, @eugene)
     assert_equal rental_log, @dock.rental_log
+  end
+
+  def test_dock_charge_kayak
+    charge_result = { :card_number => "4242424242424242", :amount => 40 }
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @kayak_1.add_hour
+    @kayak_1.add_hour
+    assert_equal charge_result, @dock.charge(@kayak_1)
+  end
+
+  def test_dock_charge_sup_1
+    charge_result = { :card_number => "1313131313131313", :amount => 45 }
+    @dock.rent(@sup_1, @eugene)
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+    assert_equal charge_result, @dock.charge(@sup_1)
+  end
+
+  def test_dock_log_hours
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    assert_equal [1, 1], [@kayak_1.hours_rented, @kayak_2.hours_rented]
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    assert_equal [2, 2, 1], [@kayak_1.hours_rented, @kayak_2.hours_rented, @canoe.hours_rented]
+  end
+
+  def test_dock_return_boats_calculate_revenue
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    assert_equal 0, @dock.revenue
+    @dock.return(@kayak_1)
+    @dock.return(@kayak_2)
+    @dock.return(@canoe)
+    assert_equal 105, @dock.revenue
+    @dock.rent(@sup_1, @eugene)
+    @dock.rent(@sup_2, @eugene)
+    5.times { @dock.log_hour }
+    @dock.return(@sup_1)
+    @dock.return(@sup_2)
+    assert_equal 195, @dock.revenue
   end
 
 end
